c9ef82700272bf44881983d02fe70b4fc2c653f9
aldrsun <147417224+aldrsun@users.noreply.github.com>
2025-01-24 00:58:18 +0300

    Update Sieve of Eratosthenes.cpp

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -1,13 +1,11 @@
-#include <stdio.h>
+#define LIMIT 1000000000
 
-#define LIMIT 100000000
-
- bool is_not_prime[LIMIT];
+bool is_not_prime[LIMIT];
 
 int main() {
-  for(int i = 2; i < LIMIT; i ++)
-    if(is_not_prime[i] == 0)
-        for(int i2 = 2*i; i2 < LIMIT; i2 += i)
-            is_not_prime[i2] = 1;
-  return 0;
+    for(int i = 2; i < LIMIT; i ++)
+        if(is_not_prime[i] == 0)
+            for(int i2 = 2*i; i2 < LIMIT; i2 += i)
+                is_not_prime[i2] = true;
+    return 0;
 }
