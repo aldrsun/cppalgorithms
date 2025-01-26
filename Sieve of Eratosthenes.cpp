9241f9e637ad07d337dacaa42d1c731a5801b21c
aldrsun <ali.dursun@metu.edu.tr>
2025-01-27 00:17:01 +0300

    Intendation fixed.

--- Sieve of Eratosthenes.cpp
+++ Sieve of Eratosthenes.cpp
@@ -6,6 +6,6 @@ int main() {
   for(int i = 2; i < LIMIT; i ++)
     if(is_not_prime[i] == 0)
       for(int i2 = 2*i; i2 < LIMIT; i2 += i)
-            is_not_prime[i2] = 1;
+        is_not_prime[i2] = 1;
   return 0;
 }
